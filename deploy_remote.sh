1c4b9fb16b9e5c0531684ba19ce47c31c977a0f5
Koji <>
2026-03-30 00:09:48 +0900

    rename prd_compose_deploy.sh

---
+++ deploy_remote.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: "${SSH_KEY_PATH:?}"
+: "${TARGET_INSTANCE:?}"
+: "${TARGET_INFRA_DIR:?}"
+
+ssh -i "${SSH_KEY_PATH}" -t "${TARGET_INSTANCE}" \
+  "cd ${TARGET_INFRA_DIR} && ./prd_compose_deploy.sh"
