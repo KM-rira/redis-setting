4867197957c002f20c7701b0c62a5665c3a563e8
Koji <>
2026-03-30 18:54:30 +0900

    update

--- prd_compose_deploy.sh
+++ prd_compose_deploy.sh
@@ -8,6 +8,17 @@ docker compose pull
 echo "🚀 Recreate only updated services..."
 docker compose up -d --no-deps --force-recreate growry linkhub
 
+echo "⏳ Waiting for PostgreSQL to be ready..."
+
+# postgresコンテナ内で接続可能になるまで待つ
+until docker compose exec -T postgres pg_isready -U appuser > /dev/null 2>&1; do
+  sleep 2
+  echo "⏳ waiting..."
+done
+
+echo "🛠 Running DB init script..."
+docker compose exec -T growry npm run db:init
+
 echo "📦 Status:"
 docker compose ps
 
